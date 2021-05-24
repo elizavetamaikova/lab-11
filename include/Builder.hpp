69296565ca6c8d97052b88117f271a0ed0cae8ea
Liza Maykova <elizavetabmstu@yandex.ru>
2021-05-25 00:17:38 +0300

    1

---
+++ include/Builder.hpp
@@ -0,0 +1,30 @@
+// Copyright 2021 <elizavetamaikova>
+
+#ifndef INCLUDE_BUILDER_HPP_
+#define INCLUDE_BUILDER_HPP_
+
+#include <async++.h>
+#include <iostream>
+#include <boost/thread.hpp>
+#include <boost/thread/thread.hpp>
+#include <string>
+#include <boost/process.hpp>
+
+class Builder {
+ public:
+  Builder(std::string config, bool install, bool pack, int32_t timeout);
+
+  void Stop(async::cancellation_token &c);
+
+  bool NewTask(std::string task);
+
+
+ private:
+  std::string _config;
+  bool _install;
+  bool _pack;
+  int32_t _timeout;
+  boost::process::child _child;
+};
+
+#endif // INCLUDE_BUILDER_HPP_
